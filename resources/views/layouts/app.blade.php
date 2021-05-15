347af5c4626119f335ca50f43527ae979cb3c97b
Arne Ziegert <ziegert@myswooop.de>
2021-05-16 01:05:42 +0200

    commented dead links, added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link rel="icon" href="favicon.png" type="image/gif" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
