5bd73b038a9a008db9524565c1432938ba2257b8
Arne Ziegert <ziegert@myswooop.de>
2021-05-16 16:51:02 +0200

    color header bar

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -23,10 +23,6 @@
     <link rel="icon" href="favicon.png" type="image/gif" />
     <!-- Scrollbar Custom CSS -->
     <link rel="stylesheet" href="/css/jquery.mCustomScrollbar.min.css">
-    <!-- Tweaks for older IEs-->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 
 
     <!-- Google Tag Manager -->
