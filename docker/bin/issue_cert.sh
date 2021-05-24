985d57ab6e222dcf871ef2a58143cf3f8992938e
Arne Ziegert <ziegert@myswooop.de>
2021-05-24 03:58:12 +0200

    ssl refresh in cron

--- docker/bin/issue_cert.sh
+++ docker/bin/issue_cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/acme/acme.sh --installcert -d dart-vaders.eu \
---keypath /etc/nginx/ssl/letsencrypt/dart-vaders.eu/dart-vaders.eu.key \
---fullchainpath /etc/nginx/ssl/letsencrypt/dart-vaders.eu/dart-vaders.eu.cer \
---reloadcmd 'nginx -s reload'
+/acme/acme.sh --issue -d dart-vaders.eu -w /var/www/dartVaders/public \
+&& cp /root/.acme.sh/dart-vaders.eu/fullchain.cer /etc/nginx/ssl/letsencrypt/dart-vaders.eu/dart-vaders.eu.cer \
+&& cp /root/.acme.sh/dart-vaders.eu/dart-vaders.eu.key /etc/nginx/ssl/letsencrypt/dart-vaders.eu/dart-vaders.eu.key \
+&& nginx -s reload
--- docker/entrypoints/rundev.sh
+++ docker/entrypoints/rundev.sh
@@ -7,6 +7,8 @@ npm install && npm run watch &
 php artisan migrate
 #php artisan optimize
 
+crond -f &
+
 echo "starting nginx"
 nginx -c /etc/nginx/nginx.conf
 
